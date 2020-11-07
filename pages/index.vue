1898c1c0bbaa1d5cb3536fcc6ae2ecb5e6adb5b8
eir73 <roflaneir73@gmail.com>
2020-11-07 20:59:45 +0200

    add front-end part of registration form

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,67 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+        <v-card-title>
+          EIR
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
 
-      <v-btn @click='message'>
-        NEW MESSAGE
-      </v-btn>
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Room"
+              required
+            ></v-text-field>
 
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              @click="submit"
+            >
+              Enter
+            </v-btn>
+
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {
-  sockets: {
-    connect() {
-      console.log('client io connected')
-    }
-  },
-  methods: {
-    message() {
-      this.$socket.emit('createMessage', {
-        text: 'from client '
-      })
-    }
+  export default {
+    layout: 'empty',
+    sockets: {
+      connect() {
+        console.log("client io connected");
+      },
+    },
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 16) || 'Name must be less than 16 characters',
+      ],
+      room: '',
+      roomRules: [
+        v => !!v || 'Введите комнату',
+      ],
+    }),
+    methods: {
+      submit () {
+        this.$refs.form.validate()
+      },
+    },
   }
-};
 </script>
